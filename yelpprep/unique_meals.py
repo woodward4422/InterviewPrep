d53b503710676ef0ff4b6f9c9f7b63bac9828d08
noah <noahwoodward2244@gmail.com>
2018-10-28 17:58:48 -0700

    Working on frozensets stuff

--- yelpprep/unique_meals.py
+++ yelpprep/unique_meals.py
@@ -8,11 +8,15 @@ class Meal:
 # Takes in a list of meals 
 def unique_meals(meals_list):
     for element in meals_list:
-        ingredients = element.ingredients
+       frozenset(ingredients) = element.ingredients
         ingredients.sort()
+        
+    return len(ingredients_dict)
+    
+    
        # element.ingredients.sort(key = lambda s: s.ingredients)
 
     
 if __name__ == "__main__":
     meals = [Meal("American",["lettuce","cheese","olives","tomato"]),Meal("Mexican",["lettuce","cheese","pepper","tomato"]), Meal("French",["lettuce","cheese","pepper","tomato"]),Meal("Continental",["lettuce","cheese","olives","tomato"])]
-    unique_meals(meals)
+    print(unique_meals(meals))
