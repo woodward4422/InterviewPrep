14ea0662c5341cd679338d439b8f4c3eca059fed
noah <noahwoodward2244@gmail.com>
2018-10-28 18:06:32 -0700

    Finished unique meals challenge

--- yelpprep/unique_meals.py
+++ yelpprep/unique_meals.py
@@ -5,16 +5,20 @@ class Meal:
     def __init__(self,name,ingredients):
         self.name = name
         self.ingredients = ingredients
-# Takes in a list of meals 
+# Takes in a list of meals and will use a dictionary with keys being ingredients to find unique meals
+ingredients_dict = {}
 def unique_meals(meals_list):
+    if len(meals_list == 0):
+        return 0
+
     for element in meals_list:
-       frozenset(ingredients) = element.ingredients
+        ingredients = element.ingredients
         ingredients.sort()
-        
+        keys = frozenset(ingredients)
+        ingredients_dict[keys] = element.name
     return len(ingredients_dict)
     
     
-       # element.ingredients.sort(key = lambda s: s.ingredients)
 
     
 if __name__ == "__main__":
