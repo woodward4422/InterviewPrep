20a9c60cb6465dcd7bbb009bde07f0abfede47c4
noah <noahwoodward2244@gmail.com>
2018-10-28 17:45:09 -0700

    added a sort

--- yelpprep/unique_meals.py
+++ yelpprep/unique_meals.py
@@ -8,9 +8,11 @@ class Meal:
 # Takes in a list of meals 
 def unique_meals(meals_list):
     for element in meals_list:
-        element.ingredients.sort(key = lambda s: s.ingredients)
-    print(element.ingredients)
+        ingredients = element.ingredients
+        ingredients.sort()
+       # element.ingredients.sort(key = lambda s: s.ingredients)
 
     
 if __name__ == "__main__":
     meals = [Meal("American",["lettuce","cheese","olives","tomato"]),Meal("Mexican",["lettuce","cheese","pepper","tomato"]), Meal("French",["lettuce","cheese","pepper","tomato"]),Meal("Continental",["lettuce","cheese","olives","tomato"])]
+    unique_meals(meals)
