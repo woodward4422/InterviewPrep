d9414872ce605cee506018b9ba5c48058dd3c8ae
noah <noahwoodward2244@gmail.com>
2018-10-28 17:08:47 -0700

    Finished Median

--- yelpprep/median_rating.py
+++ yelpprep/median_rating.py
@@ -7,11 +7,27 @@ class Business:
     
 def median_rating_business(businesses):
     businesses.sort(key = lambda b: b.rating)
+    return median(businesses)
+
+def median(lst):
+    n = len(lst)
+    if n%2 == 1:
+        return lst[n // 2]
+    else:
+        before_middle = lst[ n // 2 - 1].rating
+        print(before_middle)
+        after_middle = lst[ n // 2 + 1].rating
+        print(after_middle)
+        return (before_middle + after_middle) / 2
+
+
+    
+
     
 
 
     
 if __name__ == "__main__":
-    businesses = [Business("Burger King", 3),Business("Barnes and Nobles", 4),Business("Subway", 2), Business("Home Depot", 5), Business("Marshalls", 3), Business("MacD",1), Business("Home Goods", 2)]
+    businesses = [Business("Burger King", 3),Business("Barnes and Nobles", 4),Business("Subway", 2), Business("Home Depot", 5), Business("Marshalls", 3), Business("MacD",1), Business("Home Goods", 2), Business("Sup Homie", 4)]
     print(median_rating_business(businesses))
 
